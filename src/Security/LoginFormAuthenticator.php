725564fa8aaf100abb81aa8dfff1041a0b313ae8
CBaraka <combo.baraka01@gmail.com>
2020-07-22 10:53:09 +0200

    ajout liens réseaux sociaux de l'asso DMDA

--- src/Security/LoginFormAuthenticator.php
+++ src/Security/LoginFormAuthenticator.php
@@ -99,8 +99,8 @@ class LoginFormAuthenticator extends AbstractFormLoginAuthenticator implements P
         // For example : return new RedirectResponse($this->urlGenerator->generate('some_route'));
         // throw new \Exception('TODO: provide a valid redirect inside '.__FILE__);
 
-        // redirect à la page d'acceuil
-        return new RedirectResponse($this->urlGenerator->generate('homepage'));
+        // redirection tableau de board admin
+        return new RedirectResponse($this->urlGenerator->generate('admin'));
     }
 
     protected function getLoginUrl()
