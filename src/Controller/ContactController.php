001de916ca0e4c62cde38ecfb7907102f141c309
CBaraka <combo.baraka01@gmail.com>
2020-08-11 21:32:11 +0200

    debut email sans la bdd

--- src/Controller/ContactController.php
+++ src/Controller/ContactController.php
@@ -7,27 +7,50 @@ use App\Form\ContactType;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\HttpFoundation\Request;
-use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Mailer\MailerInterface;
+use Symfony\Component\Mime\Email;
+
 
 class ContactController extends AbstractController
 {
     /**
      * @Route("/contact", name="contact")
      * @param Request $request
-     * @return Response
+     * @param MailerInterface $mailer
      */
-    public function index(Request $request):Response
+    public function index(Request $request , MailerInterface $mailer)
     {
-      $contact = new Contact();
-      $form = $this->createForm(ContactType::class , $contact);
+      //$contact = new Contact();
+      $form = $this->createForm(ContactType::class ); //$contact
       $form->handleRequest($request);
       if($form->isSubmitted()&& $form->isValid()){
-          $em = $this->getDoctrine()->getManager();
+        $contact = $form->getData();
+       
 
-          $em->persist($contact);
-          $em->flush();
-          return $this->redirectToRoute('homepage');
-      }
+          //ici nous enverrons le mail 
+          $email = (new Email())
+         
+        ->from(['Email'])
+          // On attribue le destinataire
+          ->To('combo.baraka@gmail.com')
+          // On crée le texte avec la vue
+          ->subject('Time for Symfony Mailer!')
+          ->text($this->renderView(
+            'emails/contact.html.twig', compact('contact')
+        ),
+        'text/html');
+      $mailer->send($email);
+          // Permet un message flash de renvoi
+      $this->addFlash('message', 'Votre message a été transmis, nous vous répondrons dans les meilleurs délais.'); 
+      return $this->redirectToRoute('homepage');
+        }
+
+
+ // $em = $this->getDoctrine()->getManager();
+
+          //$em->persist($contact); $em->flush();
+          
+      
         return $this->render('contact/index.html.twig', [
             'pagetitle' => 'ContactController',
             'form'=>$form->createView()
