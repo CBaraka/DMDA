f4110d91bee18110e3cc7bec266d375eae7ecc7d
CBaraka <combo.baraka01@gmail.com>
2020-08-12 16:11:17 +0200

    affichage description depuis la bdd sur la homepage

--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Entity\DescAsso;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -13,8 +14,10 @@ class DefaultController extends AbstractController
      */
     public function index()
     {
+        $description = $this->getDoctrine()->getRepository(DescAsso::class)->findAll();
         return $this->render('homepage/index.html.twig', [
             'pagetitle' => "Association DMDA Mayotte",
+            'description' => $description,
         ]);
     }
 }
