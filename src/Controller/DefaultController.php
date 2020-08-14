2fb9eefcec9d3eaeba4875e8f7b85f423d2ca7c8
Nassiry Mina <aminanassiry@gmail.com>
2020-08-14 21:20:30 +0200

    formulaire commentaire+entity+controller+ajout form sur hommepage

--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -3,8 +3,12 @@
 namespace App\Controller;
 
 use App\Entity\DescAsso;
+use App\Entity\Commentaire;
+use App\Form\CommentaireType;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
 
 
 class DefaultController extends AbstractController
@@ -14,11 +18,45 @@ class DefaultController extends AbstractController
      */
     public function index()
     {
+        return $this->render('homepage/index.html.twig', [
+            'pagetitle' => "Association DMDA Mayotte"
+        ]);
+    }
+    /**
+     * @Route("/", name="homepage")
+     */
+    public function description()
+    {
         $description = $this->getDoctrine()->getRepository(DescAsso::class)->findAll();
         return $this->render('homepage/index.html.twig', [
+            'description' => $description
+        ]);
+    }
+
+    /** 
+     * @Route("/", name="homepage") 
+     *@param Request $request
+     *@return Response
+     */
+    public function commentaire(Request $request): Response
+    {
+        $comment = $this->getDoctrine()->getRepository(Commentaire::class)->findAll();
+        $commentaire = new Commentaire();
+        $form = $this->createForm(CommentaireType::class, $commentaire);
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+            // Hydrate notre commentaire avec la date et l'heure courants
+            $commentaire->setCreatedAt(new \DateTime('now'));
+            $em = $this->getDoctrine()->getManager();
+
+            $em->persist($commentaire);
+            $em->flush();
+            return $this->redirectToRoute('homepage');
+        }
+        return $this->render('homepage/index.html.twig', [
             'pagetitle' => "Association DMDA Mayotte",
-            'description' => $description,
-          
+            'form' => $form->createView(),
+            'comment' => $comment
         ]);
     }
 }
---
+++ src/Form/CommentaireType.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Commentaire;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class CommentaireType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+        ->add('pseudo', TextType::class)
+        ->add('email', EmailType::class)
+        ->add('contenu',TextareaType::class)
+        ->add('rgpd', CheckboxType::class, [
+            'label' => 'J\'accepte que mes informations soient stockées dans la base de données de Mon Blog pour la gestion des commentaires. J\'ai bien noté qu\'en aucun cas ces données ne seront cédées à des tiers.'
+        ])
+        ->add('Envoyer', SubmitType::class);
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'data_class' => Commentaire::class,
+        ]);
+    }
+}
